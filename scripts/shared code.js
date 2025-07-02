e52ba1687e7792e12b1d7772829659f3fd5f6ce6
MuhammadFD-hub <fardeendar30@proton.me>
2025-07-02 19:17:17 +0500

    fix: placed correct link name due to renaming file amazon -> index

--- scripts/shared code.js
+++ scripts/shared code.js
@@ -1,46 +1,41 @@
-import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
-import { cartTotalQty } from '../data/cart.js';
+import dayjs from "https://unpkg.com/dayjs@1.11.10/esm/index.js";
+import { cartTotalQty } from "../data/cart.js";
 
-export function priceFormat(priceCents)
-{
-  return "$"+(priceCents/100).toFixed(2)
+export function priceFormat(priceCents) {
+  return "$" + (priceCents / 100).toFixed(2);
 }
-export function getId(element)
-{
+export function getId(element) {
   return element.dataset.productId;
 }
 export function renameAttribute(arr, oldName, newName) {
   if (!Array.isArray(arr)) {
-    console.error('The first argument must be an array of objects.');
+    console.error("The first argument must be an array of objects.");
     return [];
   }
 
-  // Iterate over each object in the array
-  arr.forEach(obj => {
+  arr.forEach((obj) => {
     if (obj.hasOwnProperty(oldName)) {
-      // Create the new attribute with the value of the old attribute
       obj[newName] = obj[oldName];
-      // Delete the old attribute
       delete obj[oldName];
     } else {
-      console.warn(`Attribute "${oldName}" does not exist on one of the objects.`);
+      console.warn(
+        `Attribute "${oldName}" does not exist on one of the objects.`
+      );
     }
   });
 
   return arr;
 }
 
-export function dateFormat(date)
-{
-  return dayjs(date).format('MMMM D');
+export function dateFormat(date) {
+  return dayjs(date).format("MMMM D");
 }
 
 export function deepCopyArray(arr) {
   return JSON.parse(JSON.stringify(arr));
 }
 
-export function cartIconQtyUpdate()
-{
-  let cartIcon=document.querySelector(".js-cart-quantity");
-  cartIcon.innerHTML=cartTotalQty();
+export function cartIconQtyUpdate() {
+  let cartIcon = document.querySelector(".js-cart-quantity");
+  cartIcon.innerHTML = cartTotalQty();
 }
